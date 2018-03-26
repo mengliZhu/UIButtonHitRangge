7d471434b63531c17813b02bf3168e429acd43e9
Alex <zhumengli@zuoyebang.com>
2018-03-26 10:16:28 +0800

    add file

---
+++ UIButton+TouchArea.h
@@ -0,0 +1,16 @@
+//
+//  UIButton+TouchArea.h
+//  DailyPopThreads
+//
+//  Created by zhuchao on 13-7-15.
+//  Copyright (c) 2013年 Cencent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface UIButton (TouchArea)
+
+@property(nonatomic, assign) UIEdgeInsets hitTestEdgeInsets;
+
+@end
